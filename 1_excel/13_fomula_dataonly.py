f45b5bf9e272c19d68a52514aa10564113406160
Nam Jueun <45748632+namjueun@users.noreply.github.com>
2021-03-01 02:09:58 +0900

    2021.02.29

---
+++ 1_excel/13_fomula_dataonly.py
@@ -0,0 +1,18 @@
+from openpyxl import load_workbook
+
+#수식 그대로 가져옴
+# wb = load_workbook("sample_fomula.xlsx")
+# ws = wb.active
+
+# for row in ws.values:
+#     for cell in row:
+#         print(cell)
+
+# 수식이 아닌 실제 데이터를 가지고 옴
+# evaluate 되지 않은 상태의 데이터는 None이라 나옴 None 없앨려면 열어서 다시 저장하면 됨=/ 
+wb = load_workbook("sample_fomula.xlsx" , data_only=True)
+ws = wb.active
+
+for row in ws.values:
+    for cell in row:
+         print(cell)
