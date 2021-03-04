0fadfada8f817a1a4342ac7af6be677564eb96ae
Nam Jueun <45748632+namjueun@users.noreply.github.com>
2021-03-05 08:42:28 +0900

    2021.03.05

--- 1_excel/13_fomula_dataonly.py
+++ 1_excel/13_fomula_dataonly.py
@@ -9,7 +9,7 @@ from openpyxl import load_workbook
 #         print(cell)
 
 # 수식이 아닌 실제 데이터를 가지고 옴
-# evaluate 되지 않은 상태의 데이터는 None이라 나옴 None 없앨려면 열어서 다시 저장하면 됨=/ 
+# evaluate 되지 않은 상태의 데이터는 None이라 나옴 None 없앨려면 열어서 다시 저장하면 됨 
 wb = load_workbook("sample_fomula.xlsx" , data_only=True)
 ws = wb.active
 
